89af02944877bac44595f9cc2d7053fd0aa62c1c
Olumide Akinkunmi <mide.akinkunmi@gmail.com>
2026-02-26 01:49:20 +0000

    fix: simplify CORS configuration in app.js

--- app.js
+++ app.js
@@ -15,14 +15,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5500;
 
-app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: '*',
-    credentials: false,
-  })
-);
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
