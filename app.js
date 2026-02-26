b9ac2189241e1fc0376bf220db2290910a18151d
Olumide Akinkunmi <mide.akinkunmi@gmail.com>
2026-02-26 01:22:57 +0000

    fix: update allowedHeaders in CORS configuration to accept all headers

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+    allowedHeaders: '*',
     credentials: false,
   })
 );
